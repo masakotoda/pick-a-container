8ee22fadaed809c6d18bc5f1ad606efa09c2a59f
Masako Toda <todamasako0421@hotmail.com>
2017-09-10 19:56:00 -0700

    added new report to compare 2 results

---
+++ pick.a.container.rpt/app.js
@@ -0,0 +1,30 @@
+﻿
+var app = new Vue({
+    el: '#app',
+    data: {
+        arg1: {
+            keyType: "integer",
+            valueSize: "4",
+            constructType: "once",
+            lookupType: "cacheFriendly",
+        },
+        arg2: {
+            keyType: "short_string",
+            valueSize: "4",
+            constructType: "sporadic",
+            lookupType: "sporadic",
+        },
+    },
+    methods: {
+        refreshChart(chartNum) {
+            let arg = chartNum == 1 ? this.arg1 : this.arg2;
+            console.log(arg.keyType);
+            console.log(arg.valueSize);
+            console.log(arg.constructType);
+            console.log(arg.lookupType);
+            drawAllCharts(chartNum, arg);
+        },
+    },
+    computed: {
+    }
+})
